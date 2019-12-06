d81bcda804deab6fbb47eab6f396667b74421794
kim-seungtae <tae1303@gmail.com>
2019-12-07 01:05:47 +0900

    [ADD] 댓글 많은 뉴스 기사와 댓글 가져오기

---
+++ sports/mostCommentsArticle.py
@@ -0,0 +1,62 @@
+import time, os
+import pandas as pd
+from selenium import webdriver
+
+class TrendCrawler:
+    def __init__(self):
+        self.data = None
+
+    def crawling(self):
+        # 데이터 저장 디렉토리 생성
+        save_dir = "data"
+        if not os.path.exists(save_dir):
+            os.mkdir(save_dir)
+
+        # 홈 URL
+        URL = 'https://sports.news.naver.com/index.nhn'
+
+        # 셀레니움으로 창 열기
+        #driver는 url을 복사할 창, driver2는 복사된 링크로 들어가서 기사 내용 가져올 창
+        driver = webdriver.Chrome()
+        driver2 = webdriver.Chrome()
+        driver.get(URL)
+        #정치, 경제, IT 등
+        articles = driver.find_elements_by_css_selector("#mostCommentedNewsList > li > a")
+
+        data_list = []
+
+        for article in articles:
+            driver2.get(article.get_attribute("href"))
+            title = driver2.find_element_by_css_selector("#content > div > div.content > div > div.news_headline > h4").text
+            content = driver2.find_element_by_css_selector("#newsEndContents").text                
+            comments_elements = driver2.find_elements_by_css_selector(".u_cbox_list > li")
+            comments = []
+            for i, comment in enumerate(comments_elements):
+                if i == 5:
+                    break
+                comments.append(comment.text)
+#             like = driver2.find_element_by_css_selector("#spiLayer > div._reactionModule.u_likeit > ul > li.u_likeit_list.good > a > span.u_likeit_list_count._count").text
+#             hate = driver2.find_element_by_css_selector("#spiLayer > div._reactionModule.u_likeit > ul > li.u_likeit_list.angry > a > span.u_likeit_list_count._count").text
+#             reactions=[]
+#             reactions.append(like)
+#             reactions.append(hate)
+
+            data = {}
+            data['title'] = title
+            data['contents'] = content
+            data['comments'] = comments
+#             data['reactions'] = reactions
+            data_list.append(data)
+
+        self.data = data_list
+
+    def save_csv(self, save_path):
+        df = pd.DataFrame(self.data)
+        pd.DataFrame.to_csv(df, save_path)
+
+
+
+if __name__ == "__main__":
+    crawler = TrendCrawler()
+    crawler.crawling()
+    crawler.save_csv("data.csv")
