7cebc8a0e930ec1e4eb7fddb4d585bc98327927a
kim-seungtae <tae1303@gmail.com>
2019-12-06 19:02:16 +0900

    [UPDATE] TrendCrawler.py 주석 추가

--- korea_news_crawler/TrendCrawler.py
+++ korea_news_crawler/TrendCrawler.py
@@ -7,29 +7,34 @@ class TrendCrawler:
         self.data = None
 
     def crawling(self):
+        # 데이터 저장 디렉토리 생성
         save_dir = "data"
         if not os.path.exists(save_dir):
             os.mkdir(save_dir)
 
+        # 홈 URL
         URL = 'https://news.naver.com/'
 
+        # 셀레니움으로 창 열기
+        #driver는 url을 복사할 창, driver2는 복사된 링크로 들어가서 기사 내용 가져올 창
         driver = webdriver.Chrome()
         driver2 = webdriver.Chrome()
         driver.get(URL)
-
+        #정치, 경제, IT 등
         categories = driver.find_elements_by_css_selector(".category_ranking > a")
 
         data_list = []
 
-
+        
         for category in categories:
             category.click()
             articles = driver.find_elements_by_css_selector("#right\.ranking_contents > ul > li > a")
+            # 기사에서 제목과 내용 가져오기
             for article in articles:
                 driver2.get(article.get_attribute("href"))
                 title = driver2.find_element_by_css_selector("#articleTitle").text
                 content = driver2.find_element_by_css_selector("#articleBodyContents").text                
-                
+                # 데이터 딕셔너리 생성 후 data_list에 append
                 data = {}
                 data['category'] = category.text
                 data['title'] = title
